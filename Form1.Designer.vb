f3ef8215e633b5c79cba2edcf5699e2e69135f3b
Lee Brown <brown.lee@outlook.com>
2014-07-25 20:00:31 +0100

    first upates

---
+++ Form1.Designer.vb
@@ -0,0 +1,405 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.tbupdate = New System.Windows.Forms.TextBox()
+        Me.bset = New System.Windows.Forms.Button()
+        Me.bstart = New System.Windows.Forms.Button()
+        Me.bstop = New System.Windows.Forms.Button()
+        Me.lbcountdown = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.lbupdates = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'tbupdate
+        '
+        Me.tbupdate.Location = New System.Drawing.Point(48, 19)
+        Me.tbupdate.Name = "tbupdate"
+        Me.tbupdate.Size = New System.Drawing.Size(178, 20)
+        Me.tbupdate.TabIndex = 1
+        '
+        'bset
+        '
+        Me.bset.Location = New System.Drawing.Point(148, 98)
+        Me.bset.Name = "bset"
+        Me.bset.Size = New System.Drawing.Size(75, 23)
+        Me.bset.TabIndex = 2
+        Me.bset.Text = "Set"
+        Me.bset.UseVisualStyleBackColor = True
+        '
+        'bstart
+        '
+        Me.bstart.Location = New System.Drawing.Point(6, 188)
+        Me.bstart.Name = "bstart"
+        Me.bstart.Size = New System.Drawing.Size(75, 23)
+        Me.bstart.TabIndex = 3
+        Me.bstart.Text = "Start"
+        Me.bstart.UseVisualStyleBackColor = True
+        '
+        'bstop
+        '
+        Me.bstop.Location = New System.Drawing.Point(163, 186)
+        Me.bstop.Name = "bstop"
+        Me.bstop.Size = New System.Drawing.Size(75, 23)
+        Me.bstop.TabIndex = 4
+        Me.bstop.Text = "Stop"
+        Me.bstop.UseVisualStyleBackColor = True
+        '
+        'lbcountdown
+        '
+        Me.lbcountdown.AutoSize = True
+        Me.lbcountdown.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.lbcountdown.Location = New System.Drawing.Point(6, 14)
+        Me.lbcountdown.Name = "lbcountdown"
+        Me.lbcountdown.Size = New System.Drawing.Size(23, 25)
+        Me.lbcountdown.TabIndex = 5
+        Me.lbcountdown.Text = "0"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton3)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.tbupdate)
+        Me.GroupBox1.Controls.Add(Me.bset)
+        Me.GroupBox1.Location = New System.Drawing.Point(6, 5)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(232, 127)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Update interval"
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(12, 98)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(53, 17)
+        Me.RadioButton3.TabIndex = 8
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Hours"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(12, 75)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(62, 17)
+        Me.RadioButton2.TabIndex = 7
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Minutes"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(12, 52)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(67, 17)
+        Me.RadioButton1.TabIndex = 6
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Seconds"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(9, 22)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(33, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Time:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.lbcountdown)
+        Me.GroupBox2.Location = New System.Drawing.Point(7, 138)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(139, 42)
+        Me.GroupBox2.TabIndex = 8
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Time (seconds)"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.lbupdates)
+        Me.GroupBox3.Location = New System.Drawing.Point(152, 138)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(86, 42)
+        Me.GroupBox3.TabIndex = 9
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Total updates"
+        '
+        'lbupdates
+        '
+        Me.lbupdates.AutoSize = True
+        Me.lbupdates.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.lbupdates.Location = New System.Drawing.Point(8, 14)
+        Me.lbupdates.Name = "lbupdates"
+        Me.lbupdates.Size = New System.Drawing.Size(23, 25)
+        Me.lbupdates.TabIndex = 5
+        Me.lbupdates.Text = "0"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(3, 2)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(252, 243)
+        Me.TabControl1.TabIndex = 10
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.GroupBox1)
+        Me.TabPage1.Controls.Add(Me.GroupBox3)
+        Me.TabPage1.Controls.Add(Me.bstart)
+        Me.TabPage1.Controls.Add(Me.GroupBox2)
+        Me.TabPage1.Controls.Add(Me.bstop)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(244, 217)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Timer"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.GroupBox4)
+        Me.TabPage2.Controls.Add(Me.TextBox3)
+        Me.TabPage2.Controls.Add(Me.Button4)
+        Me.TabPage2.Controls.Add(Me.Button3)
+        Me.TabPage2.Controls.Add(Me.ListBox1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(244, 217)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Scripts"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.Label2)
+        Me.GroupBox4.Controls.Add(Me.Label3)
+        Me.GroupBox4.Controls.Add(Me.TextBox1)
+        Me.GroupBox4.Controls.Add(Me.Button1)
+        Me.GroupBox4.Controls.Add(Me.Button2)
+        Me.GroupBox4.Controls.Add(Me.TextBox2)
+        Me.GroupBox4.Location = New System.Drawing.Point(7, 6)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(231, 71)
+        Me.GroupBox4.TabIndex = 11
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Add scripts"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Directory:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(20, 42)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(38, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Name:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(64, 13)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(101, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(171, 11)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(51, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Select"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(171, 36)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(51, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Add"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(64, 39)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(101, 20)
+        Me.TextBox2.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox3.Cursor = System.Windows.Forms.Cursors.Default
+        Me.TextBox3.Enabled = False
+        Me.TextBox3.Location = New System.Drawing.Point(7, 160)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(231, 20)
+        Me.TextBox3.TabIndex = 8
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(135, 186)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(103, 23)
+        Me.Button4.TabIndex = 7
+        Me.Button4.Text = "Refresh list"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(7, 186)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(103, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Remove"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(7, 85)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(231, 69)
+        Me.ListBox1.TabIndex = 3
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(257, 247)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.Text = "Py Launcher 0.1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents tbupdate As System.Windows.Forms.TextBox
+    Friend WithEvents bset As System.Windows.Forms.Button
+    Friend WithEvents bstart As System.Windows.Forms.Button
+    Friend WithEvents bstop As System.Windows.Forms.Button
+    Friend WithEvents lbcountdown As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox3 As System.Windows.Forms.GroupBox
+    Friend WithEvents lbupdates As System.Windows.Forms.Label
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox4 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,101 @@
+﻿Public Class Form1
+    Private Sub bstart_Click(sender As Object, e As EventArgs) Handles bstart.Click
+        Timer1.Enabled = True
+    End Sub
+    Private Sub bset_Click(sender As Object, e As EventArgs) Handles bset.Click
+        Timer1.Enabled = False
+        If RadioButton1.Checked = True Then
+            lbcountdown.Text = Val(tbupdate.Text)
+        Else
+
+        End If
+        If RadioButton2.Checked = True Then
+            lbcountdown.Text = Val(tbupdate.Text * 60)
+        Else
+            
+        End If
+
+        If RadioButton3.Checked = True Then
+            lbcountdown.Text = Val(tbupdate.Text * 3600)
+        Else
+
+        End If
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If lbcountdown.Text = 1 Then
+            lbcountdown.Text = Val(tbupdate.Text)
+
+            Dim MyFolder As String
+            Dim MyFile As String
+            MyFolder = "C:\py"
+            MyFile = Dir(MyFolder & "\*.py")
+            Do While MyFile <> ""
+                Process.Start(fileName:=MyFolder & "\" & MyFile)
+                MyFile = Dir()
+            Loop
+
+            lbupdates.Text = lbupdates.Text + 1
+        End If
+        If lbcountdown.Text = 0 Then
+            Timer1.Enabled = False
+            lbcountdown.Text = lbcountdown.Text + 1
+        End If
+        lbcountdown.Text = Val(lbcountdown.Text) - 1
+    End Sub
+    Private Sub bstop_Click(sender As Object, e As EventArgs) Handles bstop.Click
+        Timer1.Enabled = False
+        lbcountdown.Text = 0
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        If (Not System.IO.Directory.Exists("C:\py")) Then
+            System.IO.Directory.CreateDirectory("C:\py")
+        End If
+
+
+        Dim folderInfo As New IO.DirectoryInfo("c:\py")
+        Dim arrFilesInFolder() As IO.FileInfo
+        Dim fileInFolder As IO.FileInfo
+        arrFilesInFolder = folderInfo.GetFiles("*.*")
+        For Each fileInFolder In arrFilesInFolder
+            ListBox1.Items.Add(fileInFolder.Name)
+        Next
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        OpenFileDialog1.ShowDialog()
+        TextBox1.Text = OpenFileDialog1.FileName
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim A As String
+        A = "C:\py\" & TextBox2.Text & ".py"
+        FileCopy(TextBox1.Text, A)
+
+        ListBox1.Items.Clear()
+        Dim folderInfo As New IO.DirectoryInfo("c:\py")
+        Dim arrFilesInFolder() As IO.FileInfo
+        Dim fileInFolder As IO.FileInfo
+        arrFilesInFolder = folderInfo.GetFiles("*.*")
+        For Each fileInFolder In arrFilesInFolder
+            ListBox1.Items.Add(fileInFolder.Name)
+        Next
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        System.IO.File.Delete(TextBox3.Text)
+        ListBox1.Items.Remove(ListBox1.SelectedItem)
+    End Sub
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ListBox1.Items.Clear()
+        Dim folderInfo As New IO.DirectoryInfo("c:\py")
+        Dim arrFilesInFolder() As IO.FileInfo
+        Dim fileInFolder As IO.FileInfo
+        arrFilesInFolder = folderInfo.GetFiles("*.*")
+        For Each fileInFolder In arrFilesInFolder
+            ListBox1.Items.Add(fileInFolder.Name)
+        Next
+    End Sub
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        TextBox3.Text = "C:\py\" & ListBox1.SelectedItem
+    End Sub
+End Class
